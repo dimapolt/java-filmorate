5f4d492b1f3e0cd022bbfc4ecf19163c8899d5e3
dimapolt <dimapolt@yandex.ru>
2023-12-10 20:21:56 +0700

    style: check style

--- src/main/java/ru/yandex/practicum/filmorate/utils/FilmRateExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/utils/FilmRateExceptionHandler.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.utils;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
