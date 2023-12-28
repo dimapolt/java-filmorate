8a33d29fff0e1f9f2acb15eb0ff148e10d768631
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2023-12-28 12:41:42 +0300

    feat: создал класс ReviewDbStorage для реализации методов интерфейса ReviewStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewDbStorage.java
@@ -0,0 +1,66 @@
+package ru.yandex.practicum.filmorate.storage.review;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Review;
+
+import java.util.List;
+import java.util.Objects;
+
+@Component
+public class ReviewDbStorage implements ReviewStorage {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public ReviewDbStorage(JdbcTemplate jdbcTemplateArg) {
+        jdbcTemplate = jdbcTemplateArg;
+    }
+
+    @Override
+    public Review createReview(Review review) {
+        return null;
+    }
+
+    @Override
+    public Review updateReview(Review review) {
+        return null;
+    }
+
+    @Override
+    public List<Review> getAllReviews() {
+        return null;
+    }
+
+    @Override
+    public Review getReviewById(int id) {
+        return null;
+    }
+
+    @Override
+    public void deleteReviewById(int id) {
+
+    }
+
+    @Override
+    public void addLikeForFilmReview(int reviewId, int userId) {
+
+    }
+
+    @Override
+    public void addDislikeForFilmReview(int reviewId, int userId) {
+
+    }
+
+    @Override
+    public void deleteLikeForFilmReview(int reviewId, int userId) {
+
+    }
+
+    @Override
+    public void deleteDislikeForFilmReview(int reviewId, int userId) {
+
+    }
+}
