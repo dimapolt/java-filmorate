c97b71e7f594657d2f4a944c6bb36be1a2f9a478
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2023-12-28 13:57:05 +0300

    feat: в пакет controller добавлен класс ReviewController с методом createReview

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -0,0 +1,32 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.service.ReviewService;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/reviews")
+public class ReviewController {
+    private static final Logger log = LoggerFactory.getLogger(ReviewController.class); // создаем логгер
+    private final ReviewService reviewService;
+
+    @Autowired
+    public ReviewController(ReviewService reviewServiceArg) {
+        reviewService = reviewServiceArg;
+    }
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED) // устанавливаем код ответа 201
+    public Review createReview(@Valid @RequestBody Review review) {
+        log.info("Пришел POST /reviews запрос с телом: {}", review);
+        final Review createdReview = reviewService.createReview(review);
+        log.info("На запрос POST /reviews отправлен ответ с телом: {}",createdReview);
+        return createdReview;
+    }
+}
