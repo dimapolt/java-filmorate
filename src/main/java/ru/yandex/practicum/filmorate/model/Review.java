1a7147d3f92645e8f52d080a425d31cfc871d3ca
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2023-12-28 12:14:20 +0300

    feat: добавил в пакет model новый класс Review

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -0,0 +1,43 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Review {
+    private long reviewId;
+
+    @NotNull
+    private String content;
+    private boolean isPositive;
+
+    @Positive
+    private long userId;
+
+    @Positive
+    private long filmId;
+    private int useful;
+
+    public Review(long reviewIdArg, String contentArg, boolean isPositiveArg,
+                  long filmIdArg, long userIdArg, int usefulArg) {
+        reviewId = reviewIdArg;
+        content = contentArg;
+        isPositive = isPositiveArg;
+        filmId = filmIdArg;
+        userId = userIdArg;
+        useful = usefulArg;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" + "reviewId=" + reviewId + ", content.length='" + content.length() + '\'' +
+                ", isPositive=" + isPositive + ", userId=" + userId + ", filmId=" + filmId + ", useful=" + useful +
+                "}" + '\n';
+    }
+}
