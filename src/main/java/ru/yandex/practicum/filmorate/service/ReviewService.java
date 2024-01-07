71596bfe2f72dcf656da8a6973b6e19dc7c18718
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-01-07 16:26:04 +0300

    feat: в класс ReviewService добавил методы addLikeOrDislikeForFilmReview и deleteLikeOrDislikeForFilmReview

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -3,10 +3,12 @@ package ru.yandex.practicum.filmorate.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exceptions.NoDataFoundException;
 import ru.yandex.practicum.filmorate.model.Review;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.review.ReviewStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+import ru.yandex.practicum.filmorate.exceptions.ReviewNotFoundException;
 
 @Service
 public class ReviewService {
@@ -40,13 +42,57 @@ public class ReviewService {
     }
 
     public Review getReviewById(long id) {
-        return reviewStorage.getReviewById(id); // метод выбрасывает NoDataException если пользователь не найден по id
+        return reviewStorage.getReviewById(id); // метод выбрасывает NoDataException если отзыв не найден по id
     }
 
     public void deleteReviewById(long id) {
         Review deletedReview = reviewStorage.getReviewById(id); // получаем отзыв на фильм из БД перед удалением
         reviewStorage.deleteReviewById(id); // удаляем отзыв на фильм из БД
         // добавляем удаление отзыва в ленту событий
+    }
+
+    public void addLikeOrDislikeForFilmReview(long reviewId, long userId, boolean isLike) {
+        try {
+            // проверяем на наличие отзыва на фильм в БД
+            reviewStorage.getReviewById(reviewId);
+            // проверяем на наличие пользователя в БД
+            userStorage.getUserById(userId);
+
+            if (isLike) {
+                // ставим лайк отзыву, т.е добавляем запись в таблицу reviews_likes
+                reviewStorage.addLikeOrDislikeForFilmReview(reviewId, userId, true);
+                // увеличиваем рейтинг полезности отзыва, обновляя поле useful в таблице reviews
+                reviewStorage.increaseUsefulForFilmReview(reviewId);
+            } else {
+                // ставим дизлайк отзыву, т.е добавляем запись в таблицу reviews_likes
+                reviewStorage.addLikeOrDislikeForFilmReview(reviewId, userId, false);
+                // уменьшаем рейтинг полезности отзыва, обновляя поле useful в таблице reviews
+                reviewStorage.decreaseUsefulForFilmReview(reviewId);
+            }
+        } catch (ReviewNotFoundException exc) {
+            String message = String.format("Отзыв с id=%d не найден в базе данных!", reviewId);
+            throw new ReviewNotFoundException(message);
+        } catch (NoDataFoundException exc) {
+            String message = String.format("Пользователь с id=%d не найден в базе данных!", userId);
+            throw new NoDataFoundException(message);
+        }
+    }
+
+    public void deleteLikeOrDislikeForFilmReview(long reviewId, long userId) {
+        try {
+            // проверяем на наличие отзыва на фильм в БД преждем удалять лайк/дизлайк
+            reviewStorage.getReviewById(reviewId);
+            // проверяем на наличие пользователя в БД прежде чем удалять лайк/дизлайк
+            userStorage.getUserById(userId);
+            // удаляем лайк/дизлайк у отзыва
+            reviewStorage.deleteLikeOrDislikeForFilmReview(reviewId, userId);
+        } catch (ReviewNotFoundException exc) {
+            String message = String.format("Отзыв с id=%d не найден в базе данных!", reviewId);
+            throw new ReviewNotFoundException(message);
+        } catch (NoDataFoundException exc) {
+            String message = String.format("Пользователь с id=%d не найден в базе данных!", userId);
+            throw new NoDataFoundException(message);
+        }
     }
 
     private void checkFilmExist(Review review) {
