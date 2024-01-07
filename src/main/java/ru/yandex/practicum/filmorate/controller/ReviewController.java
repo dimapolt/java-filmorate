c3be55f5d65c223222bcf3df1429bbae5069394f
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-01-07 16:28:37 +0300

    feat: в класс ReviewController добавил методы addLikeForFilmReview, addDislikeForFilmReview, deleteLikeForFilmReview и deleteDislikeForFilmReview

--- src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -53,4 +53,32 @@ public class ReviewController {
         reviewService.deleteReviewById(id);
         log.info("Отзыв с id={} успешно удален!", id);
     }
+
+    @PutMapping("/{id}/like/{userId}")
+    public void addLikeForFilmReview(@PathVariable long id, @PathVariable long userId) {
+        log.info("Пришел PUT /reviews/{}/like/{} запрос", id, userId);
+        reviewService.addLikeOrDislikeForFilmReview(id, userId, true);
+        log.info("Пользователь с id={} поставил лайк отзыву с id={}", userId, id);
+    }
+
+    @PutMapping("/{id}/dislike/{userId}")
+    public void addDislikeForFilmReview(@PathVariable long id, @PathVariable long userId) {
+        log.info("Пришел PUT /reviews/{}/dislike/{} запрос", id, userId);
+        reviewService.addLikeOrDislikeForFilmReview(id, userId, false);
+        log.info("Пользователь с id={} поставил дизлайк отзыву с id={}", userId, id);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void deleteLikeForFilmReview(@PathVariable long id, @PathVariable long userId) {
+        log.info("Пришел DELETE /reviews/{}/like/{} запрос", id, userId);
+        reviewService.deleteLikeOrDislikeForFilmReview(id, userId);
+        log.info("Пользователь с id={} удалил лайк у отзыва с id={}", userId, id);
+    }
+
+    @DeleteMapping("/{id}/dislike/{userId}")
+    public void deleteDislikeForFilmReview(@PathVariable long id, @PathVariable long userId) {
+        log.info("Пришел DELETE /reviews/{}/dislike/{} запрос", id, userId);
+        reviewService.deleteLikeOrDislikeForFilmReview(id, userId);
+        log.info("Пользователь с id={} удалил дизлайк у отзыва с id={}", userId, id);
+    }
 }
