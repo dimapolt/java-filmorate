ed124888f7b2d489904887da62305bb48c8d1002
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-01-09 23:19:51 +0300

    style: убрал лишние комментарии в методе updateReview класса ReviewService

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -37,10 +37,6 @@ public class ReviewService {
 
     public Review updateReview(Review review) {
 
-        // Две нижние проверки игнорируем, id фильма и пользователя для отзыва останется неизменным
-
-        //checkFilmExist(review);
-        //checkUserExist(review);
         Review updatedReview = reviewStorage.updateReview(review);
         // добавляем обновление отзыва в ленте событий
         return updatedReview;
